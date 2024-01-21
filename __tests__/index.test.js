7842ad427554ba66f9f0b21f76ad33f7bacced93
nick <ghost2394228@gmail.com>
2024-01-21 17:16:53 +0300

    upd tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,82 @@
 import test from 'node:test';
 import assert from 'assert/strict';
 
-import { countAverage, alphaOmega, fill, fromPairs, intersection } from '../index.js'
+import {
+  countAverage,
+  alphaOmega,
+  fill,
+  fromPairs,
+  intersection,
+} from '../index.js';
 
-test('task1', () => {
-    assert.strictEqual(countAverage([5, 7, 9, 3, 2]), 5.2);
+describe('task1', () => {
+  it('should count', () => {
+    expect(countAverage([5, 7, 9, 3, 2])).toEqual(5.2);
+  });
+  it('should count', () => {
+    expect(countAverage([4, 7, 9, 3, 2])).toEqual(5);
+  });
+  it('should not count', () => {
+    expect(countAverage()).toBeUndefined();
+  });
 });
-test('task2', () => {
-    assert.ok(alphaOmega([5, 7, 9, 3, 2]), [5, 2]);
-    assert.ok(alphaOmega([4, 7, 9, 3, 2]), [4, 2]);
+
+describe('task2', () => {
+  it('should find', () => {
+    expect(alphaOmega([5, 7, 9, 3, 2])).toEqual([5, 2]);
+  });
+  it('should find', () => {
+    expect(alphaOmega([4, 7, 9, 3, 2])).toEqual([4, 2]);
+  });
+  it('should not find', () => {
+    expect(alphaOmega()).toBeUndefined();
+  });
 });
-test('task3', () => {
-    assert.ok(fill(4, 'someString'), ['someString', 'someString', 'someString', 'someString']);
-    assert.ok(fill(2, ['someString']), [['someString'], ['someString']]);
+
+describe('task3', () => {
+  it('should fill', () => {
+    expect(fill(4, 'someString')).toEqual([
+      'someString',
+      'someString',
+      'someString',
+      'someString',
+    ]);
+  });
+  it('should not fill', () => {
+    expect(fill(2)).toEqual([undefined, undefined]);
+  });
 });
-test('task4', () => {
-    assert.ok(fromPairs([['a', 1], ['b', 2]]), { 'a': 1, 'b': 2 });
-    assert.ok(fromPairs([['someString', [2, 3]], ['b', 2]]), { 'someString': [2, 3], 'b': 2 });
+
+describe('task4', () => {
+  it('should pair', () => {
+    expect(
+      fromPairs([
+        ['a', 1],
+        ['b', 2],
+      ])
+    ).toEqual({ a: 1, b: 2 });
+  });
+  it('should pair', () => {
+    expect(
+      fromPairs([
+        ['someString', [2, 3]],
+        ['b', 2],
+      ])
+    ).toEqual({ someString: [2, 3], b: 2 });
+  });
+  it('should not pair', () => {
+    expect(fromPairs()).toBeUndefined();
+  });
 });
-test('task5', () => {
-    assert.ok(intersection([1, 2], [2, 3]), [2]);
-    assert.ok(intersection(['b', 'e', 'c'], ['b', 'b', 'e']), ['b', 'e']);
+
+describe('task5', () => {
+  it('should find intersection', () => {
+    expect(intersection([1, 2], [2, 3])).toEqual([2]);
+  });
+  it('should find intersection', () => {
+    expect(intersection(['b', 'e', 'c'], ['b', 'b', 'e'])).toEqual(['b', 'e']);
+  });
+  it('should not find intersection', () => {
+    expect(intersection()).toBeUndefined();
+  });
 });
--- index.js
+++
@@ -1,9 +0,0 @@
-export const countAverage = (array) => {};
-
-export const alphaOmega = (array) => {};
-
-export const fill = (number, element) => {};
-
-export const fromPairs = (array) => {};
-
-export const intersection = (...arrays) => {};
