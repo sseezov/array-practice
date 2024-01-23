ff6ba3a71f1f9a0d3616d34720d0af8cd71dfb0b
sseezov <sseezov@mail.ru>
2024-01-23 12:14:42 +0700

    test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,78 +1,79 @@
+import { describe, expect, it } from '@jest/globals';
 import * as funcs from '../index.js';
 
 /* test('task1', () => { */
-  describe('task1', () => {
-    it('should count', () => {
-      expect(funcs.countAverage([5, 7, 9, 3, 2])).toEqual(5.2);
-    });
-    it('should count', () => {
-      expect(funcs.countAverage([4, 7, 9, 3, 2])).toEqual(5);
-    });
-    it('should not count', () => {
-      expect(funcs.countAverage()).toBeUndefined();
-    });
+describe('task1', () => {
+  it('should count', () => {
+    expect(funcs.countAverage([5, 7, 9, 3, 2])).toEqual(5.2);
   });
+  it('should count', () => {
+    expect(funcs.countAverage([4, 7, 9, 3, 2])).toEqual(5);
+  });
+  it('should not count', () => {
+    expect(funcs.countAverage()).toBeUndefined();
+  });
+});
 
-  describe('task2', () => {
-    it('should find', () => {
-      expect(funcs.alphaOmega([5, 7, 9, 3, 2])).toEqual([5, 2]);
-    });
-    it('should find', () => {
-      expect(funcs.alphaOmega([4, 7, 9, 3, 2])).toEqual([4, 2]);
-    });
-    it('should not find', () => {
-      expect(funcs.alphaOmega()).toBeUndefined();
-    });
+describe('task2', () => {
+  it('should find', () => {
+    expect(funcs.alphaOmega([5, 7, 9, 3, 2])).toEqual([5, 2]);
   });
+  it('should find', () => {
+    expect(funcs.alphaOmega([4, 7, 9, 3, 2])).toEqual([4, 2]);
+  });
+  it('should not find', () => {
+    expect(funcs.alphaOmega()).toBeUndefined();
+  });
+});
 
-  describe('task3', () => {
-    it('should fill', () => {
-      expect(funcs.fill(4, 'someString')).toEqual([
-        'someString',
-        'someString',
-        'someString',
-        'someString',
-      ]);
-    });
-    it('should not fill', () => {
-      expect(funcs.fill(2)).toEqual([undefined, undefined]);
-    });
+describe('task3', () => {
+  it('should fill', () => {
+    expect(funcs.fill(4, 'someString')).toEqual([
+      'someString',
+      'someString',
+      'someString',
+      'someString',
+    ]);
   });
+  it('should not fill', () => {
+    expect(funcs.fill(2)).toEqual([undefined, undefined]);
+  });
+});
 
-  describe('task4', () => {
-    it('should pair', () => {
-      expect(
-        funcs.fromPairs([
-          ['a', 1],
-          ['b', 2],
-        ])
-      ).toEqual({ a: 1, b: 2 });
-    });
-    it('should pair', () => {
-      expect(
-        funcs.fromPairs([
-          ['someString', [2, 3]],
-          ['b', 2],
-        ])
-      ).toEqual({ someString: [2, 3], b: 2 });
-    });
-    it('should not pair', () => {
-      expect(funcs.fromPairs()).toBeUndefined();
-    });
+describe('task4', () => {
+  it('should pair', () => {
+    expect(
+      funcs.fromPairs([
+        ['a', 1],
+        ['b', 2],
+      ])
+    ).toEqual({ a: 1, b: 2 });
   });
+  it('should pair', () => {
+    expect(
+      funcs.fromPairs([
+        ['someString', [2, 3]],
+        ['b', 2],
+      ])
+    ).toEqual({ someString: [2, 3], b: 2 });
+  });
+  it('should not pair', () => {
+    expect(funcs.fromPairs()).toBeUndefined();
+  });
+});
 
-  describe('task5', () => {
-    it('should find intersection', () => {
-      expect(funcs.intersection([1, 2], [2, 3])).toEqual([2]);
-    });
-    it('should find intersection', () => {
-      expect(funcs.intersection(['b', 'e', 'c'], ['b', 'b', 'e'])).toEqual([
-        'b',
-        'e',
-      ]);
-    });
-    it('should not find intersection', () => {
-      expect(funcs.intersection()).toBeUndefined();
-    });
+describe('task5', () => {
+  it('should find intersection', () => {
+    expect(funcs.intersection([1, 2], [2, 3])).toEqual([2]);
   });
+  it('should find intersection', () => {
+    expect(funcs.intersection(['b', 'e', 'c'], ['b', 'b', 'e'])).toEqual([
+      'b',
+      'e',
+    ]);
+  });
+  it('should not find intersection', () => {
+    expect(funcs.intersection()).toBeUndefined();
+  });
+});
 /* }); */
